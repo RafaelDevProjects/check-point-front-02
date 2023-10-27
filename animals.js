dd056b60681e545cc48e30f2751f68719f1c0614
Rafael De Almeida <124459052+RafaelDevProjects@users.noreply.github.com>
2023-10-27 08:36:43 -0300

    cp-02

---
+++ animals.js
@@ -0,0 +1,2 @@
+const animals = ['macacaco','leao','sereia','baleia'];
+console.log((animals.includes) ? 'Existe um leão no arrat animals' : 'Não existe um leão no array animals')
---
+++ series.js
@@ -0,0 +1,6 @@
+const myFavoriteSeries = ['Cidade de Deus', 'Sintonia', 'Úlltima Parada'];
+
+console.log(myFavoriteSeries.length === 3) ? 'True' : 'False';
+
+
+
